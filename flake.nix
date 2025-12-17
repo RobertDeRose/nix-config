a2b75ab4cb8699f9211e15aab7ccb30a4ea0d923
Robert DeRose <rderose@checkpt.com>
2025-12-17 04:11:09 -0500

    TEMP

--- flake.nix
+++ flake.nix
@@ -1,59 +1,128 @@
 {
   description = "Rob's Nix setup for macOS and Linux";
 
-  ##################################################################################################################
-  #
-  # Want to know Nix in details? Looking for a beginner-friendly tutorial?
-  # Check out https://github.com/ryan4yin/nixos-and-flakes-book !
-  #
-  ##################################################################################################################
-
-  # the nixConfig here only affects the flake itself, not the system configuration!
-  nixConfig = {
-    substituters = [
-      # Query the mirror of USTC first, and then the official cache.
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
-  # This is the standard format for flake.nix. `inputs` are the dependencies of the flake,
-  # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
+  # This is the standard format for flake.nix. `inputs` are the dependencies of the flake, Each item in `inputs` will be
+  # passed as a parameter to the `outputs` function after being pulled and built.
   inputs = {
-    # 1. Core Packages (Nixpkgs) - Pinned to the 25.11 stable release branch
+    # Core Packages (Nixpkgs) - Pinned to the 25.11 stable release branch
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
 
-    # 2. Darwin System Config
+    # Darwin System Config
     darwin = {
       url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # 3. User Configuration
+    # Support Login Items
+    darwin-login-items.url = "github:uncenter/nix-darwin-login-items";
+
+    # User Configuration
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # mac-app-util.url = "github:hraban/mac-app-util";
-    # nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    # Flake Module management
+    flake-parts {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # GitOps for NixOS and nix-darwin
+    comin = {
+      url = "github:nlewo/comin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Easily configure hosts
+    easy-hosts.url = "github:tgirlcloud/easy-hosts";
+
+    # Install homebrew if not installed
+    homebrew = "github:zhaofengli/nix-homebrew";
+
+    # Transative deps
+    systems = "github:nix-systems/default-darwin";
+
+    # Formatter
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  # The `outputs` function will return all the build results of the flake.
-  # A flake can have many use cases and different types of outputs,
-  # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
-  # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
-  # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
+  # The `outputs` function will return all the build results of the flake. A flake can have many use cases and different
+  # types of outputs, parameters in `outputs` are defined in `inputs` and can be referenced by their names. However,
+  # `self` is an exception, this special parameter points to the `outputs` itself (self-reference) The `@` syntax here
+  # is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      inherit (inputs) systems;
+
+      imports = [
+        inputs.easy-hosts.flakeModule
+        inputs.treefmt-nix.flakeModule
+        inputs.home-manager.flakeModules.home-manager
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+        treefmt = {
+          programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
+          programs.nixfmt.package = pkgs.nixfmt-rfc-style;
+          programs.shellcheck.enable = true;
+          settings.formatter.shellcheck.options = [
+            "-s"
+            "bash"
+          ];
+        };
+      };
+
+      easy-hosts = {
+        path = ./hosts;
+
+        shared = {
+          modules = [ ./modules/base ];
+          specialArgs = { inherit inputs; };
+        };
+
+        perClass = class: {
+          modules = [ ./modules/${class}/default.nix ];
+        };
+
+        hosts = {
+          USMBDEROSER = {
+            arch = "aarch64";
+            class = "darwin";
+            tags = [ "laptop" ];
+          };
+
+          dev-som = {
+            arch = "aarch64";
+            class = "nixos";
+            tags = [ "server" ];
+          };
+
+          jammy = {
+            arch = "aarch64";
+            class = "nixos";
+            tags = [ "vm" ];
+          };
+        };
+      };
+
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+};
   outputs =
     inputs@{ self
     , nixpkgs
     , darwin
     , home-manager
-    # , mac-app-util
-    # , nix-vscode-extensions
+    # , mac-app-util , nix-vscode-extensions
     , ...
   }: let
     fullname = "Robert DeRose";
@@ -76,8 +145,6 @@
         ./modules/apps.nix
         ./modules/host-users.nix
 
-        # mac-app-util.darwinModules.default
-
         # home manager
         home-manager.darwinModules.home-manager
         {
@@ -85,8 +152,7 @@
           home-manager.useUserPackages = true;
           home-manager.verbose = true;
           home-manager.extraSpecialArgs = specialArgs;
-          # home-manager.sharedModules = [
-          #   mac-app-util.homeManagerModules.default
+          # home-manager.sharedModules = [ mac-app-util.homeManagerModules.default
           # ];
           home-manager.users.${username} = import ./home;
         }
